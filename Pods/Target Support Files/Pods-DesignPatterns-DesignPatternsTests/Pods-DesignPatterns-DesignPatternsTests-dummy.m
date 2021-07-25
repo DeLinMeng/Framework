957637557ad810dee9ad54d8c35f26d7e9f992ca
delin <2352915584@qq.com>
2021-07-25 18:36:35 +0800

    fix

---
+++ Pods/Target Support Files/Pods-DesignPatterns-DesignPatternsTests/Pods-DesignPatterns-DesignPatternsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DesignPatterns_DesignPatternsTests : NSObject
+@end
+@implementation PodsDummy_Pods_DesignPatterns_DesignPatternsTests
+@end
